3a3948caf5791276ab897bd21bd2948ad8184f71
Le Tran <lethanhbaotran.letran@gmail.com>
2022-09-01 11:00:35 +0700

    AddNewMission

---
+++ WeekSchedule/WeekSchedule/Form2.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace WeekSchedule
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NewMission = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.Addbtn = new System.Windows.Forms.Button();
+            this.MissionTxt = new System.Windows.Forms.TextBox();
+            this.NameRole = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.NewMission.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // NewMission
+            // 
+            this.NewMission.Controls.Add(this.panel1);
+            this.NewMission.Location = new System.Drawing.Point(12, 12);
+            this.NewMission.Name = "NewMission";
+            this.NewMission.Size = new System.Drawing.Size(512, 150);
+            this.NewMission.TabIndex = 0;
+            this.NewMission.TabStop = false;
+            this.NewMission.Text = "Nhiệm vụ mới";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dateTimePicker1);
+            this.panel1.Controls.Add(this.Addbtn);
+            this.panel1.Controls.Add(this.MissionTxt);
+            this.panel1.Controls.Add(this.NameRole);
+            this.panel1.Location = new System.Drawing.Point(6, 30);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(500, 114);
+            this.panel1.TabIndex = 0;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(194, 3);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(300, 31);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // Addbtn
+            // 
+            this.Addbtn.Location = new System.Drawing.Point(183, 77);
+            this.Addbtn.Name = "Addbtn";
+            this.Addbtn.Size = new System.Drawing.Size(112, 34);
+            this.Addbtn.TabIndex = 2;
+            this.Addbtn.Text = "Thêm";
+            this.Addbtn.UseVisualStyleBackColor = true;
+            this.Addbtn.Click += new System.EventHandler(this.Addbtn_Click);
+            // 
+            // MissionTxt
+            // 
+            this.MissionTxt.Location = new System.Drawing.Point(3, 40);
+            this.MissionTxt.Name = "MissionTxt";
+            this.MissionTxt.PlaceholderText = "Nhiệm vụ";
+            this.MissionTxt.Size = new System.Drawing.Size(491, 31);
+            this.MissionTxt.TabIndex = 1;
+            // 
+            // NameRole
+            // 
+            this.NameRole.Location = new System.Drawing.Point(3, 3);
+            this.NameRole.Name = "NameRole";
+            this.NameRole.PlaceholderText = "Tên vai trò";
+            this.NameRole.Size = new System.Drawing.Size(185, 31);
+            this.NameRole.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 168);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1035, 314);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1059, 494);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.NewMission);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.NewMission.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private GroupBox NewMission;
+        private Panel panel1;
+        private Button Addbtn;
+        private TextBox MissionTxt;
+        private TextBox NameRole;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private DateTimePicker dateTimePicker1;
+    }
+}
---
+++ WeekSchedule/WeekSchedule/Form2.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeekSchedule
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            InitSTT();
+            InitMissionSTT();
+            InitMission();
+        }
+
+        //Var declaration
+        static int stt = 0, missionstt = 0;
+        static string sttstring="";
+
+        GroupBox[] gbox = new GroupBox[1000];
+        FlowLayoutPanel[] fpanel = new FlowLayoutPanel[1000];
+        CheckBox[] ckbox = new CheckBox[100000]; 
+
+        private void InitSTT()
+        {
+            if(!File.Exists("NumOfRole.txt"))
+            {
+                File.WriteAllText("NumOfRole.txt", "0");
+                stt = 0;
+            }
+            else
+            {
+                sttstring = File.ReadAllText("NumOfRole.txt");
+                stt = Convert.ToInt32(sttstring);
+            }
+        }
+
+        private void InitMissionSTT()
+        {
+            if (!File.Exists("NumOfMission.txt"))
+            {
+                File.WriteAllText("NumOfMission.txt", "0");
+                missionstt = 0;
+            }
+            else
+            {
+                sttstring = File.ReadAllText("NumOfMission.txt");
+                missionstt = Convert.ToInt32(sttstring);
+            }
+        }
+
+        private void InitMission()
+        {
+            
+        }
+
+        private int RoleNum(string s)
+        {
+            foreach(GroupBox item in flowLayoutPanel1.Controls)
+            {
+                if (item.Text == s) return Convert.ToInt32(item.Name);
+            }
+            return -1;
+        }
+
+        private void CreateNewGb(int n)
+        {
+            ///Save to Directory
+            if(!Directory.Exists("Role"+n.ToString()))  Directory.CreateDirectory("Role" + n.ToString());
+
+            ///Save To File Name
+            if(!File.Exists("Role" + n.ToString()+"/Name.txt"))
+                File.WriteAllText("Role" + n.ToString() + "/Name.txt", NameRole.Text);
+            
+            ///CreateNewGroupBox
+            GroupBox gb = new GroupBox()
+            {
+                Name = n.ToString(),
+                Text = NameRole.Text,
+                Size = new Size(404, 252)
+            };
+
+            FlowLayoutPanel fp = new FlowLayoutPanel()
+            {
+                Name = n.ToString(),
+                AutoScroll = true,
+                Size = new Size(392, 216),
+                Location = new Point(gb.Location.X + 10, gb.Location.Y + 25)
+            };
+
+            gbox[n] = gb;
+            fpanel[n] = fp;
+
+            gb.Controls.Add(fp);
+            flowLayoutPanel1.Controls.Add(gb);
+        }
+
+        private void CreateNewMission(int n)
+        {
+            CheckBox ckb = new CheckBox()
+            {
+                AutoSize = true,
+                Text = MissionTxt.Text + ", " + dateTimePicker1.Text
+            };
+
+            ///Save to file Mission
+            if (!File.Exists("Role" + n.ToString() + "/Mission.txt"))
+                File.WriteAllText("Role" + n.ToString() + "/Mission.txt", ckb.Text);
+            else File.AppendAllText("Role" + n.ToString() + "/Mission.txt", ckb.Text);
+
+            ckbox[missionstt] = ckb;
+            fpanel[n].Controls.Add(ckb);
+        }
+
+        private void Addbtn_Click(object sender, EventArgs e)
+        {
+            ///Check NameRole & Mission
+            if(NameRole.Text=="" || NewMission.Text=="")
+            {
+                MessageBox.Show("Hãy nhập tên vai trò và nhiệm vụ mới.", "Thông báo",
+                    MessageBoxButtons.OK,MessageBoxIcon.Warning);
+                return;
+            }
+
+            ///Add new namerole
+            int kt=RoleNum(NameRole.Text);
+            if(kt==-1)
+            {
+                stt++;
+                CreateNewGb(stt);
+                kt = stt;
+            }
+            missionstt++;
+            CreateNewMission(kt);
+        }
+    }
+}
